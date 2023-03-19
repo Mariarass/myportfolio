9d8ff25f52657d74e30c0e4329f3946c29c5a207
Mariarass <mariarass2001@gmail.com>
2023-03-19 17:32:25 +0400

    ref: change project

--- src/components/CardProject/CardProject.jsx
+++ src/components/CardProject/CardProject.jsx
@@ -1,32 +1,42 @@
 import React, {forwardRef, useState} from 'react';
 import s from './CardProject.module.css'
-import styled from "styled-components";
-import travel from '../../assets/travel.png'
 import {motion} from "framer-motion";
+import {ReactComponent as Git} from '../../assets/git.svg'
+import {ReactComponent as Link} from '../../assets/link.svg'
 
+export const CardProject = forwardRef(({card}, ref) => {
 
-export const CardProject = forwardRef(({card
-                                       }, ref) => {
-
-    const [linkMode, setLinkMode] = useState(false)
 
     return (
         <div ref={ref} className={s.cardContainer}>
+            <div className={s.img_container}>
+                <div className={s.circle_container}>
+                    <div className={s.circle}></div>
+                    <div className={s.circle}></div>
+                    <div className={s.circle}></div>
+                </div>
+                <img src={card.img} className={s.img}/>
+            </div>
 
-            <img src={card.img != '' ? card.img : travel} className={linkMode ? s.img + ' ' + s.imgHover : s.img}
-                 onMouseEnter={() => setLinkMode(true)}
+            <div className={s.description}>
+                <h3 className={s.header}>{card.header}</h3>
+                <p>{card.description}</p>
 
-            />
-            {linkMode && <LinkContainer showed={linkMode}
-                                        onMouseLeave={() => setLinkMode(false)}>
-                <a href={card.linkDemo} className={s.link} target={"_blank"}>
-                    Demo
-                </a>
-                <a href={card.linkGit} className={s.link} target={"_blank"}>
-                    Code
-                </a>
 
-            </LinkContainer>}
+                <div className={s.stack_container}>
+                    {card.stack.map(el => <p >{el}</p>)}
+                </div>
+
+                <div className={s.linkContainer}>
+                    <p className={s.link_text}>Code <a href={card.linkGit} target={'_blank'}><Git
+                        className={s.svg}/></a></p>
+                    <p className={s.link_text}>Live Demo <a href={card.linkDemo} target={'_blank'}><Link
+                        className={s.svg}/></a>
+                    </p>
+
+                </div>
+
+            </div>
 
 
         </div>
@@ -34,36 +44,4 @@ export const CardProject = forwardRef(({card
 })
 
 export const MCardProject = motion(CardProject)
-
-const LinkContainer = styled.div`
-  display: ${props => (props.showed ? 'flex' : 'none')};;
-  position: absolute;
-  border-radius: 20px;
-  justify-content: center;
-  height: 200px;
-  width: 300px;
-  gap: 20px;
-  background-color: rgba(23, 43, 87, 0.34);
-  align-items: center;
-  transition: 0.1s ease-in-out;
-  animation: show 0.2s 1;
-
-  @media (max-width: 400px) {
-    width: 280px;
-  }
-
-  @keyframes show {
-    0% {
-      opacity: 0;
-
-    }
-
-    100% {
-      opacity: 1;
-
-    }
-  }
-
-
-`
 
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -17,104 +17,121 @@ import {animation} from "../../utils/animation/animation";
 
 export const Projects = () => {
     return (
-        <motion.div
-            initial='hidden'
-            whileInView='visible'
-            viewport={{amount: 0.2, once: true}}
-            className={s.container}
-            id='projects'>
+        <div className={s.container}>
 
 
             <div className={s.projectContainer}>
                 <div className={s.headerContainer}>
                     <motion.h3
+                        initial='hidden'
+                        whileInView='visible'
+                        viewport={{amount: 0.4, once: true}}
                         custom={1} variants={animation}
+                        id='projects'
                         className={s.header}>
                         MY<br/>PROJECTS
+
                     </motion.h3>
                 </div>
 
-                <motion.div
-                    initial='hidden'
-                    whileInView='visible'
-                    viewport={{amount: 0.2, once: true}}
-                    className={s.skills}>
+                <div className={s.skills}>
 
                     {ListCardProject.map((el, index) => <MCardProject
-
+                        initial='hidden'
+                        whileInView='visible'
+                        viewport={{amount: 0.3, once: true}}
                         key={index}
                         card={el}
-                        custom={index + 2} variants={animation}/>)}
+                        variants={animation}/>)}
 
-                </motion.div>
+                </div>
             </div>
-        </motion.div>
+        </div>
     );
 };
 
 
 const ListCardProject = [
     {
-        header: 'cards',
+        header: 'card training',
         img: cards,
         linkDemo: 'https://chub7.github.io/friday-project/',
         linkGit: 'https://github.com/chub7/friday-project',
-        stack: ''
+        stack: ['React', 'TypeScript', 'Redux Toolkit', 'Axios', 'Formik', 'Material UI',],
+        description: 'You can create your own learning cards in the app and share them with other students.' +
+            ' The project also implements registration, ' +
+            'login, password recovery, card filtering, card search',
+        account: ['accounttest@gmail.com', 'accounttest']
     },
     {
-        header: 'Travelanywhere',
+        header: 'website for tour',
         img: travel,
         linkDemo: 'https://www.travelanywherekey.com/about',
         linkGit: 'https://github.com/Mariarass/travelanywherekeyweb',
-        stack: ''
+        stack: ['React', 'TypeScript', 'Redux Toolkit', 'Axios', 'Formik', 'Material UI',],
+        description: 'Commercial project for viewing and ordering tours and tickets in the UAE.'
     },
     {
         header: 'todolist',
         img: todo,
         linkDemo: 'https://mariarass.github.io/todolist/',
         linkGit: 'https://github.com/Mariarass/todolist',
-        stack: ''
+        stack: ['React', 'TypeScript', 'Redux Toolkit', 'Axios', 'Material UI',],
+        description: 'Educational project. The project implements deleting, adding, editing, filtering todo list.'
+    },
+    {
+        header: 'social network',
+        img: social,
+        linkDemo: 'https://mariarass.github.io/socialnetwork/#/profile',
+        linkGit: 'https://github.com/Mariarass/socialnetwork',
+        stack: ['React', 'TypeScript', 'Redux', 'Axios', 'Formik', 'Material UI',],
+        description: 'Educational project.' +
+            ' The social network has the ability to add other members as friends, update the avatar, ' +
+            'add posts, view the profile of other members, communicate in a general chat.'
     },
     {
         header: 'instagram',
         img: insta,
         linkDemo: 'https://mariarass.github.io/insta/',
         linkGit: 'https://github.com/Mariarass/insta',
-        stack: ''
+        stack: ['Vue', , 'Pinia', 'AntDesign',],
+        description: 'The project mini instagram implemented registration and login. ' +
+            'It is also possible to search, subscribe to other members and view their profile,' +
+            ' add posts to the profile and view the friends feed.'
     },
     {
-        header: 'pokemon',
+        header: 'pokemon card',
         img: pokemon,
         linkDemo: 'https://mariarass.github.io/pokemon/',
         linkGit: 'https://github.com/Mariarass/pokemon',
-        stack: ''
+        stack: ['Vue', 'Vuex', 'AntDesign'],
+        description: '\n' +
+            'The Pokémon Project allows you to search and view information about Pokémon,' +
+            ' sort Pokémon by type, and add them to your Pokédox'
     },
-    {
-        header: 'socialnetwork',
-        img: social,
-        linkDemo: 'https://mariarass.github.io/socialnetwork/#/profile',
-        linkGit: 'https://github.com/Mariarass/socialnetwork',
-        stack: ''
-    },
+
     {
         header: 'calendar',
         img: calendar,
         linkDemo: 'https://mariarass.github.io/calendar/',
         linkGit: 'https://github.com/Mariarass/calendar',
-        stack: ''
+        stack: ['React'],
+        description: 'Project calendar allows you to plan things for a specific date.'
     },
     {
-        header: 'button',
+        header: 'edit button',
         img: button,
         linkDemo: 'https://mariarass.github.io/button/',
         linkGit: 'https://github.com/Mariarass/button',
-        stack: ''
+        stack: ['Java script'],
+        description: 'Project to quickly edit button styles and copy those styles'
     },
     {
         header: 'planer',
         img: planer,
         linkDemo: 'https://mariarass.github.io/planner/',
         linkGit: 'https://github.com/Mariarass/planner',
-        stack: ''
+        stack: ['React',],
+        description: 'Project for planning feed. It allows you to add photos, move, delete'
     },
 ]
